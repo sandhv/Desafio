a327d2c47d92128f818003b5454fb7e3fffb916e
Sandra Hernandez <sandy_hv5@hotmail.com>
2018-10-03 10:26:38 -0500

    Se agregaro la interfaz y el paquete

---
+++ src/Desafio/Practica.java
@@ -0,0 +1,10 @@
+package Desafio;
+
+import mx.com.sintelti.desafios.oporx.Solucion;
+
+public class Practica implements Solucion {
+    @Override
+    public String convertir(String entrada) {
+        return null;
+    }
+}
---
+++ src/mx/com/sintelti/desafios/oporx/Solucion.java
@@ -0,0 +1,11 @@
+package mx.com.sintelti.desafios.oporx;
+
+/**
+ * @author Javier Alberto Ramirez Hernandez
+ * Date: 13/03/2013
+ * Time: 01:57
+ * www.sintelti.mx
+ */
+public interface Solucion {
+    String convertir(String entrada);
+}
