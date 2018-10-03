56e7945dbb02a422ae9035acce8d35029254d2c4
Sandra Hernandez <sandy_hv5@hotmail.com>
2018-10-03 11:26:07 -0500

    Se realizaron cambios

---
+++ src/Desafio/Entrada.java
@@ -0,0 +1,12 @@
+package Desafio;
+
+public class Entrada {
+
+    public static void main(String[] args){
+
+        Practica e = new Practica();
+        System.out.println(e.getEntrada());
+
+
+    }
+}
--- src/Desafio/Practica.java
+++ src/Desafio/Practica.java
@@ -3,8 +3,20 @@ package Desafio;
 import mx.com.sintelti.desafios.oporx.Solucion;
 
 public class Practica implements Solucion {
+    private String entrada;
     @Override
     public String convertir(String entrada) {
-        return null;
+
+
+        entrada = entrada.replaceAll("o", "x");
+        if (entrada.length() == 7 || entrada.length() == 8) {
+            this.entrada = entrada;
+        }
+        return entrada;
+
+    }
+
+    public String getEntrada() {
+        return entrada;
     }
 }
